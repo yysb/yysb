b2b756d676eff23a5c7dd66b7db76954c95d8cfc
chenshiwen12 <1058792602@qq.com>
2016-09-26 15:17:35 +0800

    ddd
    dddd

---
+++ jeepRun.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class jeepRun : MonoBehaviour {
+	public GameObject obj;
+	public GameObject cc;
+	public GameObject cc1;
+	public GameObject cc2;
+	public GameObject cc3;
+	public GameObject body;
+	public GameObject tower;
+	public Transform a;
+	public Transform b;
+	public int  x=0;
+	// Use this for initialization
+	void Start () {
+
+	//zhushi
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			body.transform.Translate (-1*Time .deltaTime ,0,0,Space .World );
+			tower.transform.Rotate (0,2,0,Space .World );
+			cc.GetComponent <Transform > ().Rotate (1,0,0 );
+			cc1.GetComponent <Transform > ().Rotate (1,0,0 );
+			cc2.GetComponent <Transform > ().Rotate (1 ,0,0);
+			cc3.GetComponent <Transform > ().Rotate (1,0,0);
+			this.GetComponent <Transform > ().Translate (0,0,4*Time .deltaTime);
+		if (this.transform.position.x <= (a.position.x+30) && this.transform.position.x >= a.position.x) {
+			if (x != 360) {
+				this.transform.RotateAround  (b.transform.position, Vector3 .up , -1);
+				x++;
+			}
+
+		}
+	}
+	void LateUpdate(){
+		obj.GetComponent <Transform > ().Translate (-4*Time .deltaTime ,0,0,Space .World );
+	}
+}
